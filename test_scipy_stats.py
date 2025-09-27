1018bd92440b346c3ae864a9a57a26d08cd1765d
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2025-09-27 07:39:33 +0200

    Update test_scipy_stats.py

--- test_scipy_stats.py
+++ test_scipy_stats.py
@@ -10,5 +10,5 @@ def test_values():
         mesgrid_value = tb.value('float(X1[100,100])')
         assert np.isclose(mesgrid_value, 8.8442, atol=1e-3), f"X1[100,100]={mesgrid_value} of task 1.2 is not equal to 8.8442"
 
-        Z_value = tb.value('float(max(Z))')
+        Z_value = tb.value('float(np.max(Z))')
         assert np.isclose(Z_value, 0.00030627, atol=1e-7), f"max(Z)={Z_value} of task 1.3 is not equal to 0.00030627"
